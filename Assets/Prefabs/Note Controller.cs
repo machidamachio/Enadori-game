2a7b65d2c37bda9f9978c8225791767755edbfb8
machidamachio <wd.machida@gmail.com>
2024-09-21 09:54:58 +0900

    0000

--- Assets/Prefabs/Note Controller.cs
+++ Assets/Prefabs/Note Controller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NoteController : MonoBehaviour
 {
-    private float speed =  120;
+    private float speed =  ;
     // Start is called before the first frame update
     void Start()
     {
